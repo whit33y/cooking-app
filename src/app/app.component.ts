2a6e7380a603e802c8550552cc93a62a5d4ed869
whit33y <troppout@gmail.com>
2024-03-13 15:57:17 +0100

    Header is working now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cooking app';
+  loadedFeature = 'recipe';
+  onNavigate(feature: string) {
+    console.log(feature);
+    this.loadedFeature = feature;
+  }
 }
