b741a3b0fe584e27898ed91e09bf61b566e67268
whit33y <troppout@gmail.com>
2024-03-18 12:41:03 +0100

    Added ingredients list to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,12 +8,14 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'Just a test',
-      'https://www.cookipedia.co.uk/wiki/images/6/66/Sausage%2C_bean_and_radicchio_hash_pasta_recipe.jpg'
+      'https://www.cookipedia.co.uk/wiki/images/6/66/Sausage%2C_bean_and_radicchio_hash_pasta_recipe.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
     ),
     new Recipe(
       'A second test recipe',
       'Just a test 2',
-      'https://www.cookipedia.co.uk/wiki/images/6/66/Sausage%2C_bean_and_radicchio_hash_pasta_recipe.jpg'
+      'https://www.cookipedia.co.uk/wiki/images/6/66/Sausage%2C_bean_and_radicchio_hash_pasta_recipe.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 2)]
     ),
   ];
 
