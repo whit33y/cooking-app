8e04eada0edfa5e59788ae7a8cf0dfc2ee1acee9
whit33y <troppout@gmail.com>
2024-03-20 15:33:58 +0100

    Routing- main routes added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { NgModule } from '@angular/core';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
