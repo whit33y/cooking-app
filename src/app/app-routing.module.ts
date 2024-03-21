51f57629f0fda440ec66003f49ea7dfb454592ce
whit33y <troppout@gmail.com>
2024-03-21 15:35:17 +0100

    Routing- recipe edit component created, routes created(order is required)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   {
@@ -20,8 +21,16 @@ const appRoutes: Routes = [
         component: RecipeStartComponent,
       },
       {
+        path: 'new',
+        component: RecipeEditComponent,
+      },
+      {
         path: ':id',
         component: RecipeDetailComponent,
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
       },
     ],
   },
