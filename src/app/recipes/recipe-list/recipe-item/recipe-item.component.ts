5592411b0403fb36d41b339d9297535ff3e43cae
whit33y <troppout@gmail.com>
2024-03-21 07:32:19 +0100

    Routing- recipe details links with active class works

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,9 +8,6 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) {}
+  @Input() index: number;
   ngOnInit() {}
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
