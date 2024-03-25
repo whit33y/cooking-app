56a3446d8adf265f3509c13816618f73c9c02ae7
whit33y <troppout@gmail.com>
2024-03-25 22:38:38 +0100

    Edit form- submiting new and editing ingredients works

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,10 +33,16 @@ export class ShoppingEditComponent {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
