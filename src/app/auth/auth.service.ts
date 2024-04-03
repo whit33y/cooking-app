dcb444a7d3e36130f486187faa7668d9c7fa8207
whit33y <troppout@gmail.com>
2024-04-03 15:46:07 +0200

    Auth: error handler improved

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
   kind: string;
@@ -14,9 +16,23 @@ interface AuthResponseData {
 export class AuthService {
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYXoe1Y10ralIJYrpfWu0877jM97-a4zw',
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYXoe1Y10ralIJYrpfWu0877jM97-a4zw',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unkown error occured';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already!';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
