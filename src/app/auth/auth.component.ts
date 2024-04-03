b76d9276bc0feb63da9c10c37083f6285a58ae38
whit33y <troppout@gmail.com>
2024-04-03 15:54:11 +0200

    Auth: loging in logic added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -16,7 +17,6 @@ export class AuthComponent {
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
-
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
@@ -24,21 +24,23 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
     this.isLoading = true;
+    let authObs: Observable<AuthResponseData>;
     if (this.isLoginMode) {
-      //....
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (errorMessage) => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 }
