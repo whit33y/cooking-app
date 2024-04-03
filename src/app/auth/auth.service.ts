2725c7a981a0c45909d3fda84f96650c487bf80d
whit33y <troppout@gmail.com>
2024-04-03 18:32:39 +0200

    Auth: login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -22,24 +22,30 @@ export class AuthService {
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYXoe1Y10ralIJYrpfWu0877jM97-a4zw',
         { email: email, password: password, returnSecureToken: true }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unkown error occured';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already!';
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
+
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBYXoe1Y10ralIJYrpfWu0877jM97-a4zw',
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBYXoe1Y10ralIJYrpfWu0877jM97-a4zw',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError(this.handleError));
+  }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'Bad email or password';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'This user account has been disabled';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
