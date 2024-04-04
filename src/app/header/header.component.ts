2cd231d1b90c349df29695f8eb5170a2c193d890
whit33y <troppout@gmail.com>
2024-04-04 15:25:39 +0200

    Auth: basoc loging in session

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+
+  isAuthenticated = false;
+
   collapsed = true;
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
