0aeb8a274124b22f0ccdbcecf6c0abc346f8e129
whit33y <troppout@gmail.com>
2024-04-04 22:00:24 +0200

    Auth: interceptor and token handling

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' }) //opcjonalne, uzywamy gdy chcemy uzywac serwis w innym serwisie
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -24,6 +29,7 @@ export class DataStorageService {
       });
   }
   fetchRecipes() {
+    //take tylko 1 arg z observable
     return this.http
       .get<Recipe[]>(
         'https://cooking-app-313b6-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
