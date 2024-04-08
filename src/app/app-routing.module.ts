7be26cdb9943db0fb2b6b196bd9804bcb6a4d6fc
whit33y <troppout@gmail.com>
2024-04-08 15:27:15 +0200

    Lazy loading: recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     redirectTo: '/recipes',
     pathMatch: 'full',
+  },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((m) => m.RecipesModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { RouterModule } from '@angular/router';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -19,8 +15,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
-    RouterModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
